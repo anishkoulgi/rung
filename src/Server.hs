0da336c3d976ca62ef03d99d0d0bf78851d91c26
anishkoulgi <anish.koulgi@gmail.com>
2023-11-29 10:48:21 -0800

    refactor: converted ServerState to data and added games field

--- src/Server.hs
+++ src/Server.hs
@@ -4,31 +4,34 @@ import Control.Monad (forM_, forever)
 import Control.Exception (finally)
 import Control.Concurrent (newMVar, MVar, readMVar, modifyMVar_)
 import Data.Text (Text)
+import Data.Map (Map, empty)
 import qualified Data.Text.IO as T
 import qualified Network.WebSockets as WS
 import Utils (parseHeaders, getCodeFromHeaders, host, port)
 
 type Client = (String, WS.Connection)
-type ServerState = [Client]
+data ServerState = ServerState { clients :: [Client], games :: Map String [Client] }
 
 newServer :: ServerState
-newServer = []
+newServer = ServerState [] empty
 
 doesClientExist :: Client -> ServerState -> Bool
-doesClientExist client = any ((== fst client) . fst)
+doesClientExist client (ServerState cls _) = any ((== fst client) . fst) cls
 
 addNewClient :: Client -> ServerState -> ServerState
-addNewClient client state = if not (doesClientExist client state)
-                            then client : state
-                            else state
+addNewClient client state@(ServerState { clients = cls }) = if not (doesClientExist client state)
+                            then state { clients = client : cls } 
+                            else state 
 
 removeClient :: Client -> ServerState -> ServerState
-removeClient client = filter ((/= fst client) . fst)
+removeClient client state@(ServerState { clients = cls }) = state { clients = filteredClients }
+    where
+        filteredClients = filter ((/= fst client) . fst) cls
 
 broadcastMessage :: Text -> ServerState -> IO()
-broadcastMessage msg state = do
+broadcastMessage msg (ServerState { clients = cls }) = do
     T.putStrLn msg
-    forM_ state $ \(_, conn) -> WS.sendTextData conn msg
+    forM_ cls $ \(_, conn) -> WS.sendTextData conn msg
 
 runServer :: IO ()
 runServer = do
